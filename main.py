e51d2766619a4759580c75d7871b999272266d43
VStefanov89 <vstefanovofm@gmail.com>
2022-08-18 16:08:16 +0300

    Creating two excel files, one with colored rows and one with not. This is just for demonstrations.

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ pd.set_option('display.expand_frame_repr', False)
 
 df = Data()
 keys = input(f"Possible columns {str(df.my_df.columns)}. What columns do you want: ").split(", ")
-print(df.export_data(*keys, colored=False))
+print(df.export_data(*keys))
