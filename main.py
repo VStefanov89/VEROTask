021a0cca88867e9878cfb65c051363985d268703
VStefanov89 <vstefanovofm@gmail.com>
2022-08-18 16:00:12 +0300

    Creating our main.py file where we will instantiate our Data class and start our export method with the given keys and color flag. Also here we install Jinja2 and openpyxl packages to be able to perform our coloring.

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from data import Data
 
 
 pd.set_option('display.expand_frame_repr', False)
-my_data = Data()
-print(my_data)
+
+df = Data()
+keys = input(f"Possible columns {str(df.my_df.columns)}. What columns do you want: ").split(", ")
+
+print(df.export_data(*keys, colored=False))
+
+
+
